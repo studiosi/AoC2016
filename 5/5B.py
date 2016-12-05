0649ad19f4255d53363a69cb5f03eaefeacbca47
David Gil de Gómez Pérez <dgil@uef.fi>
2016-12-05 08:53:05 +0200

    Took away some unnecessary prints from 5B
    Cleaning...

--- 5/5B.py
+++ 5/5B.py
@@ -8,9 +8,7 @@ while "-" in pwd:
     hd = hashlib.md5(chsh + str(i)).hexdigest()
     if hd[:5] == "00000" and hd[5] in vpos:
         pos = int(hd[5])
-        print pos
         if pwd[pos] == "-":
             pwd[pos] = hd[6]
-        print pwd
     i += 1
 print "".join(pwd)    
